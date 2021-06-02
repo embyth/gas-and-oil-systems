cc4e3d5a810b8708622cdf90894dba307bc699d7
Rostyslav Miniukov <thresh1337@gmail.com>
2021-06-02 22:23:03 +0300

    Добавил компонент для тега head с seo данными

---
+++ components/head-with-seo/head-with-seo.jsx
@@ -0,0 +1,22 @@
+import Head from "next/head";
+
+const HeadWithSeo = () => (
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="assets/css/libs/normalize.css" />
+    <link rel="stylesheet" href="assets/css/style.min.css" />
+    <link rel="shortcut icon" href="./favicon.ico" type="image/x-icon" />
+    <link rel="icon" href="./favicon.ico" type="image/x-icon" />
+    <title>Технологічний розрахунок магістрального нафтопроводу</title>
+    <meta
+      name="description"
+      content="Онлайн розрахунок пропускної здатності магістрального нафтопроводу за умов заданого розташування нафтоперекачувальних станцій"
+    />
+    <meta
+      name="keywords"
+      content="онлайн, розрахувати, розрахунок, іфнтунг, ifntuog, sayneyu, нпс, гнпс, нафто, нафтоперекачувальна, нафтоперекачувальні, станції, магістральні, нафтопроводи, напір, online, середюк, ттн, ттнн, проектування, експлуатація, нафта, нафтопродукти, напору, гідравлічний, діаметри, діаметр, підбор, формула, формули, тзнг, транспортування, тиск, тисків, розташування, траса, трубопровід, режим, роботи, насос, насосні, агрегати, агрегат, нм, нмп, характеристика, технологічний, розрахунок, визначення, величин, густина, в'язкість, витрата, обладнання, модель, математична, механічний, уточнений, розстановка, пропускна, здатність"
+    />
+  </Head>
+);
+
+export default HeadWithSeo;
