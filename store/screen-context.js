5fdcae7c60f32666a22e7a310fe9c25b01cf70f7
Rostyslav Miniukov <thresh1337@gmail.com>
2021-06-05 21:48:24 +0300

    Добавил контекст экранам расчета

---
+++ store/screen-context.js
@@ -0,0 +1,62 @@
+import { createContext, useReducer, useState } from "react";
+
+const screenReducer = (state, action) => {
+  switch (action.type) {
+    case "UPDATE_STATE":
+      return action.payload;
+
+    default:
+      throw new Error(`Unexpected action type!`);
+  }
+};
+
+const ScreenContext = createContext({
+  currentScreenId: {},
+  screensInfo: {},
+  changeScreen: () => {},
+});
+
+export const ScreenContextProvider = ({ children, initialState }) => {
+  const [state, dispatch] = useReducer(screenReducer, initialState);
+
+  const initialCurrentScreen = state.length > 0 ? state[0].id : null;
+
+  const [currentScreenId, setCurrentScreenId] = useState(initialCurrentScreen);
+
+  const getScreenById = (id) => state.find((screen) => screen.id === id);
+
+  const updateScreensState = (newScreen) => {
+    const index = state.indexOf(newScreen);
+    const updatedScreen = { ...newScreen, isDisabled: false };
+
+    const updatedState = [
+      ...state.slice(0, index),
+      updatedScreen,
+      ...state.slice(index + 1),
+    ];
+
+    dispatch({ type: "UPDATE_STATE", payload: updatedState });
+  };
+
+  const changeScreenHandler = (screenId) => {
+    const targetScreen = getScreenById(screenId);
+
+    if (targetScreen.isDisabled) {
+      updateScreensState(targetScreen);
+    }
+
+    setCurrentScreenId(targetScreen.id);
+  };
+
+  const context = {
+    currentScreenId,
+    screensState: state,
+    changeScreen: changeScreenHandler,
+  };
+
+  return (
+    <ScreenContext.Provider value={context}>{children}</ScreenContext.Provider>
+  );
+};
+
+export default ScreenContext;
