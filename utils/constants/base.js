c08bbabf5c8ff87029819383864a0c5f6f8c94a1
Rostyslav Miniukov <thresh1337@gmail.com>
2021-06-05 22:01:21 +0300

    Добавил проектные константы

---
+++ utils/constants/base.js
@@ -0,0 +1,14 @@
+export const ErrorMessage = {
+  VALUE_MISSING: `Введіть дані`,
+  RANGE_OVERFLOW: `Значення не може бути більше `,
+  RANGE_UNDERFLOW: `Значення не може бути менше `,
+};
+
+export const SessionStorage = {
+  INCOME_DATA: `INCOME_DATA`,
+  STATIONS: `STATIONS`,
+  SEGMENTS: `SEGMENTS`,
+  RESULTS: `RESULTS`,
+};
+
+export const SHAKE_ANIMATION_TIMEOUT = 600;
