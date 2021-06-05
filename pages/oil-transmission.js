49aef1158f4eb6a21b516fd4683c0c218b60a049
Rostyslav Miniukov <thresh1337@gmail.com>
2021-06-05 21:58:56 +0300

    Добавил логику страницы в точку входа

--- pages/oil-transmission.js
+++ pages/oil-transmission.js
@@ -1,5 +1,38 @@
-const OilTransmissionPage = () => (
-  <div>Oil Transmission Page</div>
+import Head from "next/head";
+
+import { ScreenContextProvider } from "../store/screen-context";
+
+import Layout from "../components/layout/layout";
+import OilTransmission from "../components/oil-transmission/oil-transmission";
+
+import { getOilTransmissionProps } from "../utils/calculations";
+
+const OilTransmissionPage = ({
+  currentCalculation,
+  incomeInputFields,
+  resultFields,
+  introInfo,
+  screensInfo,
+}) => (
+  <>
+    <Head>
+      <title>Технологічний розрахунок магістрального нафтопроводу</title>
+    </Head>
+    <ScreenContextProvider initialState={screensInfo}>
+      <Layout currentCalculation={currentCalculation}>
+        <OilTransmission
+          screensInfo={screensInfo}
+          introInfo={introInfo}
+          incomeInputFields={incomeInputFields}
+          resultFields={resultFields}
+        />
+      </Layout>
+    </ScreenContextProvider>
+  </>
 );
+
+export const getStaticProps = () => ({
+  props: getOilTransmissionProps(),
+});
 
 export default OilTransmissionPage;
