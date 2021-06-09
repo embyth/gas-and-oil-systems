a54c3d7ba4b09a8bafd75e05d0e77492e82b1121
Rostyslav Miniukov <thresh1337@gmail.com>
2021-06-09 18:36:10 +0300

    Добавил контекст для данных расчета

---
+++ store/calculation-data-context.js
@@ -0,0 +1,92 @@
+import { createContext, useReducer } from "react";
+
+const calculationDataReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_INCOME_DATA":
+      return {
+        ...state,
+        INCOME_DATA: action.payload,
+      };
+    case "SET_STATIONS_DATA":
+      return {
+        ...state,
+        STATIONS_DATA: action.payload,
+      };
+    case "SET_SEGMENTS_DATA":
+      return {
+        ...state,
+        SEGMENTS_DATA: action.payload,
+      };
+    case "SET_MIDDLE_RESULTS":
+      return {
+        ...state,
+        MIDDLE_RESULTS: action.payload,
+      };
+    case "SET_RESULTS":
+      return {
+        ...state,
+        RESULTS: action.payload,
+      };
+
+    default:
+      throw new Error(`Unexpected action type!`);
+  }
+};
+
+const CalculationDataContext = createContext({});
+
+export const CalculationDataContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(calculationDataReducer, {});
+
+  const setIncomeData = (data) => {
+    dispatch({ type: "SET_INCOME_DATA", payload: data });
+  };
+
+  const setStationsData = (data) => {
+    dispatch({ type: "SET_STATIONS_DATA", payload: data });
+  };
+
+  const setSegmentsData = (data) => {
+    dispatch({ type: "SET_SEGMENTS_DATA", payload: data });
+  };
+
+  const setMiddleResults = (results) => {
+    dispatch({ type: "SET_MIDDLE_RESULTS", payload: results });
+  };
+
+  const setResults = (results) => {
+    dispatch({ type: "SET_RESULTS", payload: results });
+  };
+
+  const getIncomeData = () => state.INCOME_DATA;
+
+  const getStationsData = () => state.STATIONS_DATA;
+
+  const getSegmentsData = () => state.SEGMENTS_DATA;
+
+  const getMiddleResults = () => state.MIDDLE_RESULTS;
+
+  const getResults = () => state.RESULTS;
+
+  const context = {
+    setIncomeData,
+    setStationsData,
+    setSegmentsData,
+    setMiddleResults,
+    setResults,
+    getIncomeData,
+    getStationsData,
+    getSegmentsData,
+    getMiddleResults,
+    getResults,
+    calcState: state,
+  };
+
+  return (
+    <CalculationDataContext.Provider value={context}>
+      {children}
+    </CalculationDataContext.Provider>
+  );
+};
+
+export default CalculationDataContext;
