f1ed4af7dfb5d2c18c76b100a1918a2618658cf7
Rostyslav Miniukov <thresh1337@gmail.com>
2021-06-09 18:32:06 +0300

    Установил пакет для реакта с увидомленияви

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import ReactNotification from "react-notifications-component";
+import "react-notifications-component/dist/theme.css";
+
 import HeadWithSeo from "../components/head-with-seo/head-with-seo";
 
 const MyApp = ({ Component, pageProps }) => (
   <>
     <HeadWithSeo />
+    <ReactNotification />
     <Component {...pageProps} />
   </>
 );
