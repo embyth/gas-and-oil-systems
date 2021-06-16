5d385a2bd848c4eafebd8a3a4271c4fa19e1af93
Rostyslav Miniukov <thresh1337@gmail.com>
2021-06-16 21:27:46 +0300

    Расширил возможности компонента

--- components/income-data/income-data-item.jsx
+++ components/income-data/income-data-item.jsx
@@ -1,3 +1,5 @@
+import parse from "html-react-parser";
+
 const IncomeDataItem = ({
   definition,
   label,
@@ -6,34 +8,68 @@ const IncomeDataItem = ({
   min,
   max,
   step,
-  demention,
-  value,
+  dimension,
+  values,
   refItem,
   onInputChange,
-}) => (
-  <div className="data__item">
-    <label htmlFor={id} className="data__label">
-      {label}
-    </label>
-    <div className="data__item-input">
-      <dfn className="data__input--definition">{definition}</dfn>
-      <input
-        type="number"
-        className={`data__input data__input--${id}`}
-        id={id}
-        placeholder={placeholder}
-        min={min}
-        max={max}
-        step={step}
-        autoComplete="off"
-        required
-        value={value}
-        ref={refItem}
-        onChange={onInputChange}
-      />
-      <span className="data__input--dimension">{demention}</span>
+  isTriple = false,
+}) =>
+  isTriple ? (
+    <div className="data__item">
+      <label htmlFor={`${id}1`} className="data__label">
+        {label}
+      </label>
+      <div className="data__item--triple">
+        {placeholder.map((placeholderItem, index) => (
+          <div key={`${id}-${placeholderItem}`} className="data__item-input">
+            <dfn className="data__input--definition">
+              {parse(`${definition}<sub>${index + 1}</sub>`)}
+            </dfn>
+            <input
+              type="number"
+              className={`data__input data__input--${id}${index + 1}`}
+              id={`${id}${index + 1}`}
+              placeholder={placeholderItem}
+              min={min}
+              max={max}
+              step={step}
+              autoComplete="off"
+              required={index === 0}
+              value={values[`${id}${index + 1}`]}
+              ref={index === 0 ? refItem : null}
+              onChange={onInputChange}
+            />
+            <span className="data__input--dimension data__input--dimension-long">
+              {parse(dimension)}
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  ) : (
+    <div className="data__item">
+      <label htmlFor={id} className="data__label">
+        {label}
+      </label>
+      <div className="data__item-input">
+        <dfn className="data__input--definition">{parse(definition)}</dfn>
+        <input
+          type="number"
+          className={`data__input data__input--${id}`}
+          id={id}
+          placeholder={placeholder}
+          min={min}
+          max={max}
+          step={step}
+          autoComplete="off"
+          required
+          value={values[id]}
+          ref={refItem}
+          onChange={onInputChange}
+        />
+        <span className="data__input--dimension">{parse(dimension)}</span>
+      </div>
+    </div>
+  );
 
 export default IncomeDataItem;
