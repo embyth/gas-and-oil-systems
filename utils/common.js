52043e063299c575e201718406fd05fc29ded56b
Rostyslav Miniukov <thresh1337@gmail.com>
2021-06-21 22:56:36 +0300

    Добавил утилитарные функции

--- utils/common.js
+++ utils/common.js
@@ -8,4 +8,14 @@ export const arrayToObjectByKey = (array, key) =>
   );
 
 export const getRandomNumber = (min, max) =>
-  Math.floor(Math.random() * (max - min)) + min;
+  +Math.floor(Math.random() * (max - min)) + min;
+
+export const getRandomFloatNumber = (min, max, fix) =>
+  +(Math.random() * (max - min) + min).toFixed(fix);
+
+export const getRandomIndex = (number) => Math.floor(Math.random() * number);
+
+export const getRandomValue = (array) => array[getRandomIndex(array.length)];
+
+export const kebabToCamelCase = (string) =>
+  string.replace(/-./g, (x) => x[1].toUpperCase());
