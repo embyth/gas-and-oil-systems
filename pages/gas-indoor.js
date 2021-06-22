e7328185510d75893ee7e31ef1b7cec7e4af42e4
Rostyslav Miniukov <thresh1337@gmail.com>
2021-06-22 20:06:20 +0300

    Добавил страницу для расчета внутридомовых газовых систем

--- pages/gas-indoor.js
+++ pages/gas-indoor.js
@@ -1,5 +1,47 @@
-const GasIndoorPage = () => (
-  <div>Gas Indoor Page</div>
+import Head from "next/head";
+
+import { ScreenContextProvider } from "../store/screen-context";
+import { CalculationDataContextProvider } from "../store/calculation-data-context";
+
+import Layout from "../components/layout/layout";
+import GasIndoor from "../components/gas-indoor/gas-indoor";
+
+import { getGasIndoorProps } from "../utils/props/collector";
+
+const GasIndoorPage = ({
+  currentCalculation,
+  screensInfo,
+  introInfo,
+  incomeInputFields,
+  segmentFields,
+  resultFields,
+}) => (
+  <>
+    <Head>
+      <title>
+        Аналітичний розрахунок газової мережі житлового будинку без застосування
+        номограм
+      </title>
+    </Head>
+    <ScreenContextProvider initialState={screensInfo}>
+      <CalculationDataContextProvider>
+        <Layout currentCalculation={currentCalculation}>
+          <GasIndoor
+            currentCalculation={currentCalculation}
+            screensInfo={screensInfo}
+            introInfo={introInfo}
+            incomeInputFields={incomeInputFields}
+            segmentFields={segmentFields}
+            resultFields={resultFields}
+          />
+        </Layout>
+      </CalculationDataContextProvider>
+    </ScreenContextProvider>
+  </>
 );
+
+export const getStaticProps = () => ({
+  props: getGasIndoorProps(),
+});
 
 export default GasIndoorPage;
