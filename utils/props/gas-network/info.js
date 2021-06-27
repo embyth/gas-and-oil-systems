6fbe534af39ccbedcfbb9064b6eba827503384bc
Rostyslav Miniukov <thresh1337@gmail.com>
2021-06-27 15:27:19 +0300

    Добавил статические пропсы для расчета ГМ

---
+++ utils/props/gas-network/info.js
@@ -0,0 +1,47 @@
+export const GAS_NETWORK_SCREENS = [
+  {
+    id: "INTRO",
+    label: "Вступ",
+    isDisabled: false,
+  },
+  {
+    id: "INCOME_DATA",
+    label: "Вихідні дані",
+    labelCropped: "Дані",
+    isDisabled: false,
+  },
+  {
+    id: "RESULTS_PHYSICS",
+    label: "Результати фіз. властивостей",
+    labelCropped: "Рез. фіз. власт.",
+    isDisabled: true,
+  },
+  {
+    id: "INCOME_CONSUMPTION",
+    label: "Витрати",
+    isDisabled: true,
+  },
+  {
+    id: "RESULTS_CONSUMPTION",
+    label: "Результати витрат",
+    labelCropped: "Рез. витрат",
+    isDisabled: true,
+  },
+  {
+    id: "INCOME_NETWORK",
+    label: "Кільця",
+    isDisabled: true,
+  },
+  {
+    id: "RESULTS_NETWORK",
+    label: "Результати кілець",
+    labelCropped: "Рез. кілець",
+    isDisabled: true,
+  },
+];
+
+export const GAS_NETWORK_INTRO_DATA = {
+  heading: `Розрахунок системи газопостачання населеного пункту (кільцева)`,
+  text: `Зовнішні мережі низького тиску (вуличні, внутрішньоквартальні, дворові газопроводи) - це складна система газопроводів, яка подає газ численним споживачам, наприклад, житловим будинкам. Витрати газу споживачами приблизно однакові, споживачі розміщені на приблизно однакових відстанях. При виконанні зазначених трьох умов для розрахунку газових мереж низького тиску може бути використана модель газопроводу з рівномірним і безперервним відбором газу по довжині.`,
+  detailsHref: `/gas-network-details`,
+};
