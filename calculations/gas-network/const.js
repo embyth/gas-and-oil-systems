76a9dfe6f1f595727a832a15c38ea66f92bb8447
Rostyslav Miniukov <thresh1337@gmail.com>
2021-06-27 15:25:47 +0300

    Добавил константы и адаптер для расчета ГМ

---
+++ calculations/gas-network/const.js
@@ -0,0 +1,172 @@
+export const AnnualNormsOfHeatConsumptionFor = {
+  GP_CGP: 2800,
+  GP_VNG: 8000,
+  GP: 4600,
+  COWS: 8820,
+  HORSES: 2120,
+  PIGS: 4620,
+};
+
+export const GAS_CONSUMPTION_FOR_HEATING_COEF = 1800;
+export const NORM_OF_TOTAL_LIVING_SPACE_PER_PERSON = 18;
+export const GAS_FURNACES_EFFICIENCY = 0.6;
+export const BOILER_EFFICIENCY = 0.8;
+export const NORMATIVE_TERM_FOR_VENTILATION = 16;
+export const GAS_CONSUMPTION_FOR_HEATING_PUBLIC_BUILDINGS_COEF = 0.25;
+
+export const IncreasedIndicatorsOfMaximumHeatFlux = {
+  OLD: {
+    "1-2": [
+      {
+        temp: -5,
+        heatFlux: 148,
+      },
+      {
+        temp: -10,
+        heatFlux: 154,
+      },
+      {
+        temp: -15,
+        heatFlux: 160,
+      },
+      {
+        temp: -20,
+        heatFlux: 205,
+      },
+      {
+        temp: -25,
+        heatFlux: 213,
+      },
+      {
+        temp: -30,
+        heatFlux: 230,
+      },
+      {
+        temp: -35,
+        heatFlux: 234,
+      },
+    ],
+    "3-4": [
+      {
+        temp: -5,
+        heatFlux: 95,
+      },
+      {
+        temp: -10,
+        heatFlux: 102,
+      },
+      {
+        temp: -15,
+        heatFlux: 109,
+      },
+      {
+        temp: -20,
+        heatFlux: 117,
+      },
+      {
+        temp: -25,
+        heatFlux: 126,
+      },
+      {
+        temp: -30,
+        heatFlux: 134,
+      },
+      {
+        temp: -35,
+        heatFlux: 144,
+      },
+    ],
+    "5+": [
+      {
+        temp: -5,
+        heatFlux: 65,
+      },
+      {
+        temp: -10,
+        heatFlux: 70,
+      },
+      {
+        temp: -15,
+        heatFlux: 77,
+      },
+      {
+        temp: -20,
+        heatFlux: 79,
+      },
+      {
+        temp: -25,
+        heatFlux: 86,
+      },
+      {
+        temp: -30,
+        heatFlux: 88,
+      },
+      {
+        temp: -35,
+        heatFlux: 98,
+      },
+    ],
+  },
+  NEW: {
+    "1-2": [
+      {
+        temp: -5,
+        heatFlux: 145,
+      },
+      {
+        temp: -10,
+        heatFlux: 152,
+      },
+      {
+        temp: -15,
+        heatFlux: 159,
+      },
+      {
+        temp: -20,
+        heatFlux: 166,
+      },
+      {
+        temp: -25,
+        heatFlux: 173,
+      },
+      {
+        temp: -30,
+        heatFlux: 177,
+      },
+      {
+        temp: -35,
+        heatFlux: 180,
+      },
+    ],
+    "5+": [
+      {
+        temp: -5,
+        heatFlux: 74,
+      },
+      {
+        temp: -10,
+        heatFlux: 80,
+      },
+      {
+        temp: -15,
+        heatFlux: 86,
+      },
+      {
+        temp: -20,
+        heatFlux: 91,
+      },
+      {
+        temp: -25,
+        heatFlux: 97,
+      },
+      {
+        temp: -30,
+        heatFlux: 101,
+      },
+      {
+        temp: -35,
+        heatFlux: 103,
+      },
+    ],
+  },
+};
